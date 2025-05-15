9d370a30de369d776a6417aca5e258daa5b32ec5
sylwo <sylwester.trzepocki@nask.pl>
2025-05-15 09:53:28 +0200

    kolejne lekcje

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,19 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // liczby całkowite
+        System.out.println(10);
+        // liczby zmiennoprzecinkowe
+        System.out.println(10.5);
+        // text
+        System.out.println("tekst do wyswietlenia");
+        // znak
+        System.out.println('s');
+        // prawda
+        System.out.println((true));
+        // fałsz
+        System.out.println((false));
+
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
